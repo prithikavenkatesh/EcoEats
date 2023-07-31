5e4e0df539ae9ee28b3a7132d64834399ecddd1d
Noga <nogah0317@gmail.com>
2023-07-31 10:11:26 -0400

    added ishika's code and fixed resources page

---
+++ EcoEats/QRCodeView.swift
@@ -0,0 +1,49 @@
+//
+//  QRCodeView.swift
+//  EcoEats
+//
+//  Created by Noga Gercsak on 7/31/23.
+//
+
+import SwiftUI
+struct QRCodeView: View {
+  var body: some View {
+      NavigationStack {
+          ZStack {
+            Color(red: 246/255, green: 240/255, blue: 214/255)
+              .ignoresSafeArea()
+            Rectangle()
+              .frame(width: 400.0, height: 350.0)
+              .offset(x:0, y:-90)
+              .foregroundColor(Color(red: 172/255, green: 214/255, blue: 87/255))
+            Image("Eco.Eats")
+              .resizable(resizingMode: .stretch)
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 300.0, height: 300.0)
+              .offset(x:0,y:-90)
+            Text("QR Code")
+              .font(.largeTitle)
+              .fontWeight(.bold)
+              .offset(x:0,y:-330)
+              .font(.system(size:100))
+            Text("This QR Code will bring you to a linktree which\n has links to websites and sources that will help decrease food waste.")
+              .font(.headline)
+              .multilineTextAlignment(.center)
+              .offset(x:0,y:150)
+            NavigationLink(destination: ContentView()) {
+              Image("homePage")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 90, height: 90)
+                .offset(x:0,y:350)
+            }
+          }
+      }
+  }
+  struct QRCodeView_Previews: PreviewProvider {
+    static var previews: some View {
+      QRCodeView()
+    }
+  }
+}
+
--- EcoEats/ResourcesView.swift
+++ EcoEats/ResourcesView.swift
@@ -6,14 +6,61 @@
 //
 
 import SwiftUI
-
 struct ResourcesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                Color(red: 246/255, green: 240/255, blue: 214/255)
+                    .ignoresSafeArea()
+                NavigationLink(destination: OrganizationsForResourcesView()) {
+                    ZStack {
+                            Capsule()
+                                .frame(width: 350.0, height: 125.0)
+                                .foregroundColor(Color(red: 172/255, green: 214/255, blue: 87/255))
+                            Text("Organizations For Donations")
+                                .foregroundColor(Color.black)
+
+                        }
+                    }
+                .offset(x:0, y:250)
+                
+                NavigationLink(destination: LearnMoreAboutFoodWasteView()) {
+                    
+                    ZStack {
+                        
+                            Capsule()
+                                .foregroundColor(Color(red: 172/255, green: 214/255, blue: 87/255))
+                                .frame(width: 350.0, height: 125.0)
+                            Text("Ways to Prevent Food Waste")
+                                .foregroundColor(Color.black)
+                        }
+                    }
+                .offset(x:0, y:25)
+                
+                NavigationLink(destination: QRCodeView()) {
+                    ZStack {
+                        
+                            Capsule()
+                                .foregroundColor(Color(red: 172/255, green: 214/255, blue: 87/255))
+                                .frame(width: 350, height: 125)
+                            Text("QR Code")
+                                .foregroundColor(Color.black)
+                            
+                    }
+                }
+                .offset(x:0, y:-200)
+                
+                Image("homePage")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 90, height: 90)
+                
+            }
+            
+        }
     }
 }
-
-struct ResourcesView_Previews: PreviewProvider {
+struct resourcesPage_Previews: PreviewProvider {
     static var previews: some View {
         ResourcesView()
     }
